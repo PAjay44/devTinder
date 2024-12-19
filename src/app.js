3e9a78741d8aefe3f8fdb9f38eb4954e71054fda
Ajay <ajaypanidapu@gmail.com>
2024-12-19 17:03:23 +0530

    exploring get,post,delete

--- src/app.js
+++ src/app.js
@@ -3,14 +3,36 @@ const express = require('express');
 
 const app = express();
 
+// app.use('/ajay', (req,res) => { // this function handles the request
+
+//     res.send('Hello I am ajay')
+// })
+
+app.get('/user', (req,res) => {
+
+    res.send({'firstname':'ajay'})
+});
+
+app.post('/user', (req,res) => {
+
+    res.send('successfully saved into database')
+});
+
+app.delete('/user', (req,res) => {
+
+    res.send('data successfully deleted in database')
+});
+
+
+
+
 app.use('/test', (req,res) => { // this function handles the request
 
     res.send('Hello from the server')
 })
 
-app.use('/ajay', (req,res) => { // this function handles the request
 
-    res.send('Hello I am ajay')
-})
+
+
 
 app.listen(3000);// listening the req on port 3000
