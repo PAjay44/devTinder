123e50fb9b5c9c1860ea3188eb6f746dcb2bfeac
Ajay <ajaypanidapu@gmail.com>
2024-12-23 22:08:18 +0530

    ' Error handling'

--- src/app.js
+++ src/app.js
@@ -2,24 +2,20 @@ const express = require("express");
 
 const app = express();
 
-const { adminAuth, userAuth } = require("./middlewares/auth");
-
-app.use("/admin", adminAuth);
-
-app.get("/admin/getAllData", (req, res) => {
-  res.send("All Data Sent");
+// Error handling
+app.get("/getUserData", (req, res) => {
+//   try {
+//     throw new Error("sshjhs");
+    res.send("user data sent");
+//   } catch (err) {
+//     res.status(500).send("some Error contact support team");
+//   }
 });
 
-app.post("/user/login", (req, res) => {
-  res.send("user logged in successfully");
-});
-
-app.get('user/data', userAuth,(req, res) => {
-  res.send("user data sent");
-});
-
-app.get("/admin/deleteUser", (req, res) => {
-  res.send(" Data deleted");
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("something went wrong");
+  }
 });
 
 app.listen(3000); // listening the req on port 3000
