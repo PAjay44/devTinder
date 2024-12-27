286a5b9478ea6c392654217125f88c6d7674a9a9
Ajay <ajaypanidapu@gmail.com>
2024-12-27 12:24:58 +0530

    get API'S

--- src/app.js
+++ src/app.js
@@ -4,12 +4,39 @@ const connectDB = require("./config/database");
 
 const app = express();
 
-const User = require('./models/user')
+const User = require("./models/user");
 
 app.use(express.json());
 
+app.get('/user', async (req, res) => {
+  const userEmail = req.body.emailId;
+
+  try {
+    const users = await User.findOne({emailId: userEmail} ); // returning promise and assigning to a user variable
+    if (users.length === 0) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+app.get('/feed', async(req, res) => {
+    try {
+        const users = await User.find({});
+        res.send(users);
+    }
+    catch (err) {
+        res.status(400).send("Something went wrong");
+      }
+
+
+})
+
 app.post("/signup", async (req, res) => {
-  const user = new User(req.body)// creating a new instance user from req body data  ;
+  const user = new User(req.body); // creating a new instance user from req body data  ;
 
   try {
     await user.save();
@@ -19,22 +46,17 @@ app.post("/signup", async (req, res) => {
   }
 });
 
-
-
 connectDB()
-.then(() => {
+  .then(() => {
     // Here we calling database connection function and connectDB will connect to the database
     // whenever it connected to DB ,it return a promise with then()
 
-    console.log('Database connection established successfully');
+    console.log("Database connection established successfully");
 
     app.listen(3000, () => {
-        console.log("server is successfully listening");
-      });
-    
+      console.log("server is successfully listening");
+    });
   })
   .catch((err) => {
-    console.log('Database cannot be connected!!')
-  })
-
-  
+    console.log("Database cannot be connected!!");
+  });
