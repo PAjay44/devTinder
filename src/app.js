04814fc06cc29f4be1cc0e603b4199f8a8702f81
Ajay <ajaypanidapu@gmail.com>
2024-12-27 17:18:34 +0530

    schema validations

--- src/app.js
+++ src/app.js
@@ -8,11 +8,11 @@ const User = require("./models/user");
 
 app.use(express.json());
 
-app.get('/user', async (req, res) => {
+app.get("/user", async (req, res) => {
   const userEmail = req.body.emailId;
 
   try {
-    const users = await User.findOne({emailId: userEmail} ); // returning promise and assigning to a user variable
+    const users = await User.findOne({ emailId: userEmail }); // returning promise and assigning to a user variable
     if (users.length === 0) {
       res.status(404).send("User not found");
     } else {
@@ -23,17 +23,40 @@ app.get('/user', async (req, res) => {
   }
 });
 
-app.get('/feed', async(req, res) => {
-    try {
-        const users = await User.find({});
-        res.send(users);
-    }
-    catch (err) {
-        res.status(400).send("Something went wrong");
-      }
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
 
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
 
-})
+  try {
+    const user = await User.findByIdAndDelete(userId);
+    res.send("user deleted successfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+
+  const data = req.body;
+
+  try {
+    const user = await User.findByIdAndUpdate({ _id: userId }, data, {
+      runValidators: true,
+    });
+    res.send("user updated successfully");
+  } catch (err) {
+    res.status(400).send("UPDATE failed:" + err.message);
+  }
+});
 
 app.post("/signup", async (req, res) => {
   const user = new User(req.body); // creating a new instance user from req body data  ;
--- src/models/user.js
+++ src/models/user.js
@@ -1,41 +1,65 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({ // creating userschema using mongoose schema
-  firstName: {
-    type: String,
-    required: true,
-  },
+const userSchema = new mongoose.Schema(
+  {
+    // creating userschema using mongoose schema
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 4,
+      maxLength: 50,
+    },
 
-  lastName: {
-    type: String,
-  },
+    lastName: {
+      type: String,
+    },
 
-  emailId: {
-    type: String,
-    required: true,
-  },
+    emailId: {
+      type: String,
+      lowercase: true,
+      required: true,
+      unique: true,
+      trim: true,
+    },
 
-  password: {
-    type: String,
-    required: true,
-  },
+    password: {
+      type: String,
+      required: true,
+    },
 
-  password: {
-    type: String,
-    required: true,
-  },
+    age: {
+      type: Number,
+      min: 18,
+    },
 
-  age: {
-    type: Number,
-  },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "other"].includes(value)) {
+          throw new Error(" gender data is not valid");
+        }
+      },
+    },
 
-  gender: {
-    type: String,
+    photoUrl: {
+      type: String,
+      default: "https://geographyandyou.com/images/user-profile.png",
+    },
+
+    about: {
+      type: String,
+      default: "This is default about the user!",
+    },
+    skills: {
+      type: [String],
+    },
   },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const User = mongoose.model("User", userSchema);
 // creating mongoose model
 // first one is name of the model and second one is schema you passed.
 module.exports = User;
-
